07c111bb9c6ba2eb5c7c40dc185e358f74fac110
Lionel Perafan <lionelperafan@gmail.com>
2022-10-05 16:29:47 -0300

    First add

---
+++ reloj.js
@@ -0,0 +1,45 @@
+const ceros = n => {
+    if (n.toString().length < 2){return "0".concat(n);}
+    else return n;
+}
+
+const actualizarHora = () => {
+    const time = new Date();
+    let hora = ceros(time.getHours());
+    let minutos = ceros(time.getMinutes());
+    let segundos = ceros(time.getSeconds());
+    document.querySelector(".hora").textContent = hora;
+    document.querySelector(".minutos").textContent = minutos;
+    document.querySelector(".segundos").textContent = segundos;
+}
+actualizarHora();
+
+setInterval(actualizarHora,1000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
